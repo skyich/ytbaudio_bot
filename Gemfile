e43123fd03bf44f15d0c4e4d4da4ac9576b66f48
Pavel Voronezhsky <pvoronezhsky@gmail.com>
2018-12-23 17:16:26 +0300

    add support for downloading large files

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@
 source "https://rubygems.org"
 
 gem "telegram-bot-ruby"
+gem "down", "~> 4.4"
 
 # gem "rails"
--- main.rb
+++ main.rb
@@ -1,7 +1,39 @@
 require 'telegram/bot'
 require 'open-uri'
+require 'down'
 
 TOKEN = '752419982:AAHdSAz39JYbXJPmHokRy_lNr2TXeAODQRY'
+
+#скачиваем и разделяем аудио на равные части по ~ 50 мб
+def split_audio(uri, song_name, size)
+  tempfile = Down.download(uri)
+  system("ffmpeg -i #{tempfile.path} -f segment -segment_time 1200 -c copy #{song_name}_part%d.mp3")
+  name = Dir["#{song_name}*"]
+  return name
+end
+
+# формируем ссылку для запроса
+def make_link(message)
+  link_reg = /v=(.*)/
+  l = message.text.match(link_reg).captures
+  return "https://www.easy-youtube-mp3.com/download.php?v=" + l[0].strip
+end
+
+# получаем всю информацию для загрузки аудио
+def get_download_info(uri)
+  source = open(uri, &:read)
+  reg_link = /<a class="btn btn-lg btn-success" href="([^"]*)"/
+  link = source.match(reg_link).captures
+  link  = link[0].strip
+  reg_size = /kbps - ([^\s]*)/
+  size = source.match(reg_size).captures
+  size = size[0].to_f
+  reg_song_name = /<title>(.*) - Easy/
+  song_name = source.match(reg_song_name).captures
+  song_name = song_name[0].downcase.gsub(/[^a-zа-я0-9\s]/i, '').strip
+  song_name.gsub!(/\s/,'_')
+  return link, size, song_name
+end
 
 loop do
   begin
@@ -16,20 +48,26 @@ loop do
             )
           else
             begin
-              link_reg = /v=(.*)/
-              l = rqst.text.match(link_reg).captures
-              l = l[0].strip
-              uri = "https://www.easy-youtube-mp3.com/download.php?v=" + l
-              puts uri
-              source = open(uri, &:read)
               bot.api.send_message(
                 chat_id: rqst.chat.id,
-                text: 'Подготовка аудио-файла...'
+                text: 'Подготовка аудио...'
               )
-              reg = /<a class="btn btn-lg btn-success" href="([^"]*)"/
-              link = source.match(reg).captures
-              link  = link[0].strip
-              bot.api.send_audio(chat_id: rqst.chat.id, audio: link)
+              link, size, song_name = get_download_info(make_link(rqst))
+              if size <= 19
+                bot.api.send_audio(chat_id: rqst.chat.id, audio: link)
+              elsif size > 19 && size < 200
+                name = split_audio(link, song_name, size)
+                name.each do |song|
+                  puts song
+                  bot.api.send_audio(chat_id: rqst.chat.id, audio: Faraday::UploadIO.new("#{song}", 'music/mp3'))
+                  File.delete(song)
+                end
+              else
+                bot.api.send_message(
+                  chat_id: rqst.chat.id,
+                  text: 'Слишком большой файл для загрузки'
+                )
+              end
             rescue
               bot.api.send_message(
                 chat_id: rqst.chat.id,
